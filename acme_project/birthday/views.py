5113406ff8361952b194b13953d3e540538b6fd6
Gleb Repin <gmrepin@mail.ru>
2025-02-07 00:10:22 +0300

    Lesson number 12

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,40 +1,67 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
 
 
-def birthday(request, pk=None):
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    else:
-        instance = None
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = '-birthday'
+    paginate_by = 5
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+class BirthdayMixIn:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayCreateView(BirthdayMixIn, CreateView):
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(BirthdayMixIn, UpdateView):
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(BirthdayMixIn, DeleteView):
+    pass
+
+# def birthday(request, pk=None):
+#     if pk is not None:
+#         instance = get_object_or_404(Birthday, pk=pk)
+#     else:
+#         instance = None
+#     form = BirthdayForm(
+#         request.POST or None,
+#         files=request.FILES or None,
+#         instance=instance
+#     )
+#     context = {'form': form}
+#     if form.is_valid():
+#         form.save()
+#         birthday_countdown = calculate_birthday_countdown(
+#             form.cleaned_data['birthday']
+#         )
+#         context.update({'birthday_countdown': birthday_countdown})
+#     return render(request, 'birthday/birthday.html', context)
+
+
+# def birthday_list(request):
+#     birthdays = Birthday.objects.order_by('birthday')
+#     paginator = Paginator(birthdays, 5)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {'page_obj': page_obj}
+#     return render(request, 'birthday/birthday_list.html', context)
+
+
+# def delete_birthday(request, pk):
+#     instance = get_object_or_404(Birthday, pk=pk)
+#     form = BirthdayForm(instance=instance)
+#     context = {'form': form}
+#     if request.method == 'POST':
+#         instance.delete()
+#         return redirect('birthday:list')
+#     return render(request, 'birthday/birthday.html', context)

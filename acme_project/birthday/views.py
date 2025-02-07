3c41c73cde20d715bc80a08260ef651148e599ba
Gleb Repin <gmrepin@mail.ru>
2025-02-07 15:40:29 +0300

    sprint 9

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
 from django.urls import reverse_lazy
+
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
@@ -8,25 +10,37 @@ from .utils import calculate_birthday_countdown
 
 class BirthdayListView(ListView):
     model = Birthday
-    ordering = '-birthday'
-    paginate_by = 5
+    ordering = 'id'
+    paginate_by = 10
 
 
 class BirthdayMixIn:
     model = Birthday
-    success_url = reverse_lazy('birthday:list')
+    form_class = BirthdayForm
 
 
 class BirthdayCreateView(BirthdayMixIn, CreateView):
-    form_class = BirthdayForm
+    pass
 
 
 class BirthdayUpdateView(BirthdayMixIn, UpdateView):
-    form_class = BirthdayForm
-
-
-class BirthdayDeleteView(BirthdayMixIn, DeleteView):
     pass
+
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
 
 # def birthday(request, pk=None):
 #     if pk is not None:

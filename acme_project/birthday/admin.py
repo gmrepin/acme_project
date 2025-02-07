7e4da7af746fcf0e2104db7206bac713aa156e67
Gleb Repin <gmrepin@mail.ru>
2025-02-07 15:40:29 +0300

    set admin

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,14 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+
+@admin.register(Birthday)
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+    list_display_links = ('first_name', 'last_name',)
+    # Разрешаю редактирование для удобства тестирования.
